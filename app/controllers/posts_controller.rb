414953be6a127b38bae492946159e9c71faafca0
knknkn1162 <knknkn1162@gmail.com>
2017-12-30 23:46:01 +0900

    impl edit and delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,7 @@ class PostsController < ApplicationController
 
   def create
     # render plain: params[:post].inspect
-    @post = Post.new(params_post)
+    @post = Post.new(post_params)
     if @post.save
       # redirect to article list
       redirect_to posts_path
@@ -23,9 +23,29 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
+  end
+
   private
 
-  def params_post
+  def post_params
     params.require(:post).permit(:title, :body)
   end
 end
